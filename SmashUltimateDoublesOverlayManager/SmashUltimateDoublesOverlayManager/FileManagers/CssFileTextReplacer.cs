29039e3e2fe1652153ba2164c3fc0124eb25c1a8
Gogoku7 <Gogoku7@users.noreply.github.com>
2018-10-28 13:34:04 +0100

    Initial commit

---
+++ SmashUltimateDoublesOverlayManager/SmashUltimateDoublesOverlayManager/FileManagers/CssFileTextReplacer.cs
@@ -0,0 +1,59 @@
+﻿using System;
+
+namespace SmashUltimateDoublesOverlayManager.FileManagers
+{
+    public class CssFileTextReplacer
+    {
+        public string ReplaceTemplateFileText(string templateCss, string cssValue)
+        {
+            if (templateCss == null)
+            {
+                throw new ArgumentException(nameof(templateCss));
+            }
+            if (string.IsNullOrEmpty(cssValue))
+            {
+                cssValue = "";
+            }
+            try
+            {
+                return templateCss.Replace("PLACEHOLDER", cssValue);
+            }
+            catch (Exception ex)
+            {
+                throw;
+            }
+        }
+
+        public string ReplaceTemplateFileTextForTeam(string cssTeamNameTextTemplateCss, string player1Sponsor, string player1Name, string player2Sponsor, string player2Name)
+        {
+            string player1FullName;
+            string player2FullName;
+            if (string.IsNullOrEmpty(player1Name))
+            {
+                player1Name = "???";
+            }
+            if (string.IsNullOrEmpty(player1Name))
+            {
+                player2Name = "???";
+            }
+            if (string.IsNullOrEmpty(player1Sponsor))
+            {
+                player1FullName = player1Name;
+            }
+            else
+            {
+                player1FullName = $"{player1Sponsor} | {player1Name}";
+            }
+            if (string.IsNullOrEmpty(player2Sponsor))
+            {
+                player2FullName = player2Name;
+            }
+            else
+            {
+                player2FullName = $"{player2Sponsor} | {player2Name}";
+            }
+
+            return cssTeamNameTextTemplateCss.Replace("PLACEHOLDER", $"{player1FullName} + {player2FullName}");
+        }
+    }
+}
